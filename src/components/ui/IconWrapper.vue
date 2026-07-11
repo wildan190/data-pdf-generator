643f5e23bacf3710dd0269350f16429efb235d95
Wildan Belfiore <mikewildan@outlook.com>
2026-07-11 17:24:30 +0700

    update

---
+++ src/components/ui/IconWrapper.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- Render FontAwesome icon -->
+  <font-awesome-icon 
+    v-if="type === 'fa'" 
+    :icon="icon" 
+    :class="iconClass" 
+  />
+  
+  <!-- Render Lucide icon -->
+  <component 
+    v-else-if="type === 'lucide'" 
+    :is="icon" 
+    :class="iconClass" 
+  />
+  
+  <!-- Fallback for regular i tag -->
+  <i v-else :class="`${type} ${icon} ${iconClass}`"></i>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface Props {
+  type: 'fa' | 'lucide' | 'fas' | 'far' | 'fab'
+  icon: string | any
+  class?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  class: ''
+})
+
+const iconClass = computed(() => props.class)
+</script>
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,31 @@ import './style.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// FontAwesome imports
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { 
+  faSync, faSyncAlt, faPlus, faDownload, faChevronDown, faCog, 
+  faFilePdf, faFileExcel, faBuilding, faExchangeAlt, faTrophy,
+  faBoxes, faExclamationTriangle, faTimesCircle, faTags, faCrown,
+  faArrowDown, faArrowUp, faChartLine, faLayerGroup
+} from '@fortawesome/free-solid-svg-icons'
+
+// Add icons to the library
+library.add(
+  faSync, faSyncAlt, faPlus, faDownload, faChevronDown, faCog,
+  faFilePdf, faFileExcel, faBuilding, faExchangeAlt, faTrophy,
+  faBoxes, faExclamationTriangle, faTimesCircle, faTags, faCrown,
+  faArrowDown, faArrowUp, faChartLine, faLayerGroup
+)
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+// Register FontAwesome component globally
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
